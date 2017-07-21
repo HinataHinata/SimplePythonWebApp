59df27efc08d5f52805ca33c16bc20aa5812b10c
ZhuChao <291324510@qq.com>
2017-07-21 17:21:16 +0800

    添加配置文件

---
+++ www/config_default.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+__author__ = '朱超'
+
+configs = {
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306
+        'user':'www-data',
+        'password':'www-data',
+        'database':'simple'
+    },
+    'session':{
+        'secret':'SiMpLe'
+    }
+}
+
+
+
